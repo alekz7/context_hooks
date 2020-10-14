df966d80aa35e5cdc7bb619c135f61f3e431db2d
alekz7 <ing.aragon.ipn@gmail.com>
2020-10-14 13:39:51 -0500

    setting up the project

---
+++ src/components/BookList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class BookList extends Component {
+    render() {
+        return (
+            <div className="book-list">
+                <ul>
+                    <li>One</li>
+                    <li>Two</li>
+                    <li>Three</li>
+                    <li>Four</li>
+                </ul>
+            </div>
+        )
+    }
+}
